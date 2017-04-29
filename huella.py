8344c0ba35775a1461356612bdcca78244523dae
RumbosN <nmrcarrasco@gmail.com>
2017-04-29 00:45:54 -0400

    se mueve para la derecha

--- huella.py
+++ huella.py
@@ -1,12 +1,13 @@
 import pygame, sys
 from pygame.locals import *
+from math import ceil
 
 def porCiento(valor, porcentaje):
 	return valor*porcentaje//100
 
 class Huella(pygame.sprite.Sprite):
 
-	def __init__(self, px, py, inicio, alpha = 255):
+	def __init__(self, px, py, alpha = 255):
 		pygame.sprite.Sprite.__init__(self)
 		# Ancho y Largo del elipse
 		self.largo = 25 
@@ -22,17 +23,14 @@ class Huella(pygame.sprite.Sprite):
 		self.px = px 
 		self.py = py 
 
-		self.time = inicio # Cambiar el Alpha de la huella
-
 		# Para que se pueda poner transparente, se pinta sobre una superficie
 		self.alpha_surface =  pygame.Surface((self.largo+porCiento(self.largo,40)+5, self.ancho), pygame.SRCALPHA, 32)  # the size of your rect
 		self.alpha_surface.set_alpha(128)
 
 
-	def mostrar(self, surface):
+	def mostrar(self, surface) :
 		#### Suela ####
 		# Datos Trapecio
-		ROJO = 0xF01818
 		sx1 = sx4 = (self.px-self.pos_ini_x) - porCiento(self.largo,10)
 		sx2 = sx3 = (self.px-self.pos_ini_x) + porCiento(self.largo,50)
 		sy2 = (self.py-self.pos_ini_y) - self.ancho//2 
@@ -58,32 +56,118 @@ class Huella(pygame.sprite.Sprite):
 		pygame.draw.rect(self.alpha_surface, self.color, t_rect)
 		surface.blit(self.alpha_surface,(self.pos_ini_x,self.pos_ini_y))
 		
-	def update_color(self, seg):
-		diferencia = seg - self.time
-		escala = 50
-		if diferencia > 0:
-			self.time = seg
-			if self.color.a - escala > 0:
-				self.color.a -= escala
+	def update_color(self):
+		escala = 33
+		if self.color.a - escala > 0:
+			self.color.a -= escala
+			return 0
+		else:
+			return 1
 			
+class Personaje(pygame.sprite.Sprite):
+	def __init__(self,surface, px=100, py=100, orientation = 0):
+		pygame.sprite.Sprite.__init__(self)
+		self.position = [px,py]
+		self.orientation = orientation
+		self.pie_izq = True
+		self.velocidad = 30
+		self.huellas = []
+		self.dif_pie = 8
+		self.ventana = surface
+		self.espera = 1
+		self.seg_pie = [2,False]
+		self.quieto = [True														# estoy quieto
+									,pygame.time.get_ticks()//500		# el ultimo segundo en el que me movi
+									,pygame.time.get_ticks()//500] 	# ultimo seg en que se borro una huella
+		h_der = Huella(self.position[0], self.position[1] + self.dif_pie)
+		h_izq = Huella(self.position[0], self.position[1] - self.dif_pie)
+		for h in [h_izq,h_der]:
+			self.huellas += [h]
+			h.mostrar(surface)
+
+	def update(self):
+		tiempo = pygame.time.get_ticks()//500	
+		actualizar = 	self.quieto[0] and (tiempo - self.quieto[2] > ceil(self.espera*1.0/3))
+		quitados = 0
+		for i in range(len(self.huellas)):
+			invisible = 0
+			if actualizar:
+				if i-quitados < len(self.huellas) - 2:
+					invisible = self.huellas[i-quitados].update_color()
+					self.quieto[2] = tiempo
+			if invisible:
+				self.huellas.pop(i-quitados)
+				quitados += 1
+			else:
+				self.huellas[i-quitados].mostrar(self.ventana)
+
+	def move_derecha(self, parado = False):
+		if not parado:
+			self.position[0] += 43
+			self.quieto[1] = pygame.time.get_ticks()//500 
+			self.quieto[0] = False
+			self.seg_pie[1] = False
+		else:
+			self.seg_pie[1] = True
+
+		self.quieto[2] = pygame.time.get_ticks()//500
+
+		if self.pie_izq :
+			dif = -1*self.dif_pie
+			self.pie_izq = False
+		else:
+			dif = self.dif_pie
+			self.pie_izq = True
+
+		huella = Huella(self.position[0], self.position[1] + dif)
+
+		longitud = len(self.huellas)
+		quitados = 0
+		for i in range(longitud):
+			if i-quitados != longitud - 1:
+				invisible = self.huellas[i-quitados].update_color()
+				if invisible:
+					self.huellas.pop(i-quitados)
+					quitados += 1
+
+		self.huellas += [huella]
+
+
+
+
 
 
 pygame.init()
 
-ancho, alto = 1360,715
+ancho, alto = 1360,730
 ventana = pygame.display.set_mode((ancho,alto))
+ventana.fill(0xFFFFFF)
 pygame.display.set_caption("Prueba")
-huella = Huella(100,100,pygame.time.get_ticks()//500)
+p1 = Personaje(ventana,100,100)
+
+# huella = Huella(100,100)
 
 while True:
+	ventana.fill(0xFFFFFF)
+
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			pygame.quit()
 			sys.exit()
+		elif event.type == pygame.KEYDOWN:
+			if event.key == K_RIGHT:
+				p1.move_derecha()
+	else:
+		if not p1.quieto[0] and (pygame.time.get_ticks()//500 - p1.quieto[1] > p1.espera):
+			p1.quieto[0] = True
 
-	ventana.fill(0xFFFFFF)
-	huella.update_color(pygame.time.get_ticks()//500)
-	huella.mostrar(ventana)
+		elif not p1.seg_pie[1] and pygame.time.get_ticks()//500 - p1.quieto[1] > p1.seg_pie[0]:
+			p1.move_derecha(True)
+
+
+	p1.update()
+	# huella.update_color()
+	# huella.mostrar(ventana)
 	#print pygame.mouse.get_pos()
 	pygame.display.update()
 
